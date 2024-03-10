58db6315edf4239d385ea6a639a3757029a8921a
Hans Pistor <62729582+hans-pistor@users.noreply.github.com>
2024-03-10 14:27:16 -0700

    Add integration test scaffolding + create uVM test (#20)
    * make code drive optional
    
    * scaffold out integration tests + implement create sandbox one
    
    * install musl-tools
    
    * Ignore test, won't run on CI
    
    ---------
    
    Co-authored-by: Hans Pistor <hans@hans-pistor.tech>

---
+++ matchbox/tests/api/sandbox/create.rs
@@ -0,0 +1,39 @@
+use matchbox::{
+    sandbox::spark::SparkClient, server::routes::sandbox::create::CreateSandboxRequest,
+};
+
+use crate::common::{ping, TestServer};
+
+#[tokio::test]
+#[ignore]
+async fn test_create_sandbox_with_no_code_drive_success() {
+    let server = TestServer::default().await;
+    let response = server
+        .create_vm(CreateSandboxRequest {
+            code_drive_path: None,
+        })
+        .await;
+
+    ping(&response.ip).expect("We should be able to ping the guest");
+
+    let mut client = SparkClient::initialize(&response.ip).await.unwrap();
+
+    assert!(
+        client.health_check().await.is_ok(),
+        "Spark health check guaranteed to succced since uVM booted"
+    );
+
+    let output = client
+        .execute(
+            "ping".into(),
+            ["-c", "1", "8.8.8.8"].map(String::from).to_vec(),
+        )
+        .await
+        .unwrap()
+        .output;
+
+    assert!(
+        output.contains("1 packets received"),
+        "We should be able to connect to the internet from the uVM"
+    );
+}
